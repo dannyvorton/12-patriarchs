5a87429d20e78a88456d93f9d287968e5396ecbf
Dan Orton <52336508+dannyvorton@users.noreply.github.com>
2023-11-05 11:52:35 -0700

    remove Angular website

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FamilyTreeComponent } from './family-tree/family-tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FamilyTreeComponent
   ],
   imports: [
     BrowserModule,
